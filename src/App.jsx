16ff01f7eb7cdc3880cef3e2a578231417b2e098
Anjana <anjanapakhrin770@gmail.com>
2025-01-07 20:41:42 +0545

    first one

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import LoginPage from "./components/LoginPage";
+
+
+
+
+function App() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
+  
